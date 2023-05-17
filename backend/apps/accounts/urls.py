76dc1c84ad56db4dad9a2420a5cd245a9b88af2d
osmanmakhtoom <osmanmakhtoom@gmail.com>
2023-05-17 22:17:33 +0330

    Add neccessary package name

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 app_name = "accounts"
 
 urlpatterns = [
-    path('api/', include("accounts.api.urls"), name="accounts-api")
+    path('api/', include("apps.accounts.api.urls"), name="accounts-api")
 ]
